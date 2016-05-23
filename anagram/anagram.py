b508624ef89a90e2f71afc22618b6129703f8314
vjchen <victor@rovertown.com>
2016-05-23 10:25:14 -0500

    First commit to Radialogica coding challenge

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -6,8 +6,8 @@ def create_anagrams(word):
     else:
         for i, character in enumerate(word):
             if not character in word[:i]: #make sure no duplication
-                for j in create_anagrams(word[:i]+word[i+1:]): #recursively switch outer characters to create anagrams
-                    yield j+character #use generator to save memory.
+                for j in create_anagrams(word[:i]+word[i+1:]): #recursively swap outer characters to create anagrams
+                    yield j+character #use generator to save memory when word gets long.
 
 
 def find_anagrams(string, word):
